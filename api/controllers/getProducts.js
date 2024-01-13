98a314d337b5bfa881549d2252376feeee19d3be
devonmartindale <devonmartindale@gmail.com>
2024-01-13 10:55:25 +0200

    first feature branch commit

---
+++ api/controllers/getProducts.js
@@ -0,0 +1,3 @@
+export const getProducts = async (req, res) => {
+  res.status(200).json({ message: "Here are your products!" });
+};
--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./routes/userRoutes.js";
+import productsRouter from "./routes/productRoutes.js";
 
 const app = express();
 
 app.use("/", userRouter);
+app.use("/", productsRouter);
 
 app.listen(5001, () => console.log("Listening on port 5001..."));
